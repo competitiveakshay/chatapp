e16cc8874827548278f0461b9e33ebb07ed9c795
Akshay <akshay@Akshays-Mac.local>
2026-06-30 23:16:19 +0530

    designing and api integeration done

--- src/components/chatSection/chatSection.jsx
+++ src/components/chatSection/chatSection.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
 import "./chatSection.css"
 import Darkmode from '../darkmode/Darkmode'
+import { IoSend } from "react-icons/io5";
 
 const ChatSection = () => {
   return (
     <div className='chatsection'>
-      <h1>Chat Section</h1>
-      <Darkmode/>
+      <div className="topsection">
+        <div className='heading'>
+          <span>
+            Hello Akshay
+          </span>
+          <span>
+            I am your AI Assistant
+          </span>
+          <span>
+            What can I help you...?
+          </span>
+        </div>
+      </div>
+
+      <div className="bottomsection">
+        <input type="text" />
+        <button id='sendbtn'><IoSend/></button>
+        <Darkmode/>
+      </div>
+
     </div>
   )
 }
